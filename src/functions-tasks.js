9f57aa9c8e887f664810840a9f978ad590fa51ee
Grigori Konopelko <grigori.konopelko@gmail.com>
2024-04-03 13:04:03 +0300

    1

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -18,7 +18,7 @@
  *
  */
 function getCurrentFunctionName() {
-  throw new Error('Not implemented');
+  return getCurrentFunctionName.name;
 }
 
 /**
@@ -50,8 +50,8 @@ function getFunctionBody(/* func */) {
  *  ]) => [0, 1, 2]
  *
  */
-function getArgumentsCount(/* funcs */) {
-  throw new Error('Not implemented');
+function getArgumentsCount(funcs) {
+  return Array.from(Array(funcs.length).keys());
 }
 
 /**
@@ -70,8 +70,10 @@ function getArgumentsCount(/* funcs */) {
  *   power05(16) => 4
  *
  */
-function getPowerFunction(/* exponent */) {
-  throw new Error('Not implemented');
+function getPowerFunction(exponent) {
+  return function unnamed(y) {
+    return y ** exponent;
+  };
 }
 
 /**
@@ -87,8 +89,14 @@ function getPowerFunction(/* exponent */) {
  *   getPolynom(8)     => y = 8
  *   getPolynom()      => null
  */
-function getPolynom() {
-  throw new Error('Not implemented');
+function getPolynom(...args) {
+  return (e) => {
+    let result = 0;
+    for (let i = 0; i < args.length; i += 1) {
+      result += args[args.length - i - 1] * e ** i;
+    }
+    return result;
+  };
 }
 
 /**
