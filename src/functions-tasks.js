9c878db44f2b2008d1cd179e61da0654800421db
Grigori Konopelko <grigori.konopelko@gmail.com>
2025-04-11 14:05:04 +0300

    feat: done

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -202,8 +202,10 @@ function logger(func, logFunc) {
  *   partialUsingArguments(fn, 'a','b','c')('d') => 'abcd'
  *   partialUsingArguments(fn, 'a','b','c','d')() => 'abcd'
  */
-function partialUsingArguments(/* fn, ...args1 */) {
-  throw new Error('Not implemented');
+function partialUsingArguments(fn, ...args1) {
+  return function newFunction(...args2) {
+    return fn(...args1, ...args2);
+  };
 }
 
 /**
@@ -223,8 +225,12 @@ function partialUsingArguments(/* fn, ...args1 */) {
  *   getId4() => 7
  *   getId10() => 11
  */
-function getIdGeneratorFunction(/* startFrom */) {
-  throw new Error('Not implemented');
+function getIdGeneratorFunction(startFrom) {
+  let id = startFrom - 1;
+  return function newFunction() {
+    id += 1;
+    return id;
+  };
 }
 
 module.exports = {
